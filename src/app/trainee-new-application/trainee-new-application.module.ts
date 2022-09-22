c9ff244fb4e381dc1c2cd2820e85ca9cf422907a
snappedrat <77161518+snappedrat@users.noreply.github.com>
2022-09-22 17:00:12 +0530

    commit 55

--- src/app/trainee-new-application/trainee-new-application.module.ts
+++ src/app/trainee-new-application/trainee-new-application.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TraineeNewApplicationRoutingModule } from './trainee-new-application-routing.module';
 import { FillFormComponent } from './fill-form/fill-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     FillFormComponent,
-    LoginFormComponent
+    LoginFormComponent,
   ],
   imports: [
     CommonModule,
-    TraineeNewApplicationRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    TraineeNewApplicationRoutingModule,
+
   ]
+
 })
 export class TraineeNewApplicationModule { }
