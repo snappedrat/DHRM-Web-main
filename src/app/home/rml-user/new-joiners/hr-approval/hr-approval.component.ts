7894d8fc01584ec4e4b10c77cda564ec08f4b5ed
snappedrat <77161518+snappedrat@users.noreply.github.com>
2022-09-26 10:32:00 +0530

    commt -hr approval

--- src/app/home/rml-user/new-joiners/hr-approval/hr-approval.component.ts
+++ src/app/home/rml-user/new-joiners/hr-approval/hr-approval.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UntypedFormGroup,UntypedFormControl, UntypedFormBuilder} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-hr-approval',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HrApprovalComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  form: any
+  filterinfo: any
+    constructor(private fb : UntypedFormBuilder, private http: HttpClient) {
+      this.form = this.fb.group({
+        status:new UntypedFormControl(' '),
+        fromdate: new UntypedFormControl(' '),
+        todate: new UntypedFormControl(' '),
+  
+      });
+     }
+    ngOnInit(): void {
+    }
+  
+  filter()
+  {
+    console.log(this.form.value)
+    this.http
+    .post('http://localhost:3000/filter', this.form.value)
+    .subscribe({
+      next: (response) =>{ console.log(response); this.filterinfo = response},
+      error: (error) => console.log(error),
+    });
   }
+  
+    traineeinfo : any[] = [
+  {
+    date: "1001",
+    fullname: "hi",
+    fathername: "hello",
+    dob: "1001",
+    mobile  : "9876543217",
+    aadhar: "444666333342",
+    apln: "appointed"
+  }
+    ]
+  
+
+
 
 }
