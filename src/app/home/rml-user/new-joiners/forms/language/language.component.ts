0fb47428f6e8a4710f8f6c60fe6d8da3525e78a1
Aameer <83020129+aameerk@users.noreply.github.com>
2022-10-11 12:54:50 +0530

    commit

--- src/app/home/rml-user/new-joiners/forms/language/language.component.ts
+++ src/app/home/rml-user/new-joiners/forms/language/language.component.ts
@@ -13,7 +13,7 @@ import { threadId } from 'worker_threads';
 })
 export class LanguageComponent implements OnInit {
 
-  
+
 
 uniqueId :any = {'mobile':''}
 details : any = []
@@ -78,42 +78,36 @@ languageList = [
 
     setTimeout(() => {
 
-    this.languageList[0].language = this.details[0]?.lang1_name
     this.languageList[0].speak = this.details[0]?.lang1_speak
     this.languageList[0].read = this.details[0]?.lang1_read
     this.languageList[0].write = this.details[0]?.lang1_write
     this.languageList[0].mothertongue = this.details[0]?.lang1_mothertounge
     this.languageList[0].understand = this.details[0]?.lang1_understand
 
-    this.languageList[1].language = this.details[0]?.lang2_name
     this.languageList[1].speak = this.details[0]?.lang2_speak
     this.languageList[1].read = this.details[0]?.lang2_read
     this.languageList[1].write = this.details[0]?.lang2_write
     this.languageList[1].mothertongue = this.details[0]?.lang2_mothertounge
     this.languageList[1].understand = this.details[0]?.lang2_understand
 
-    this.languageList[2].language = this.details[0]?.lang3_name
     this.languageList[2].speak = this.details[0]?.lang3_speak
     this.languageList[2].read = this.details[0]?.lang3_read
     this.languageList[2].write = this.details[0]?.lang3_write
     this.languageList[2].mothertongue = this.details[0]?.lang3_mothertounge
     this.languageList[2].understand = this.details[0]?.lang3_understand
 
-    this.languageList[3].language = this.details[0]?.lang4_name
     this.languageList[3].speak = this.details[0]?.lang4_speak
     this.languageList[3].read = this.details[0]?.lang4_read
     this.languageList[3].write = this.details[0]?.lang4_write
     this.languageList[3].mothertongue = this.details[0]?.lang4_mothertounge
     this.languageList[3].understand = this.details[0]?.lang4_understand
 
-    this.languageList[4].language = this.details[0]?.lang5_name
     this.languageList[4].speak = this.details[0]?.lang5_speak
     this.languageList[4].read = this.details[0]?.lang5_read
     this.languageList[4].write = this.details[0]?.lang5_write
     this.languageList[4].mothertongue = this.details[0]?.lang5_mothertounge
     this.languageList[4].understand = this.details[0]?.lang5_understand
 
-    this.languageList[5].language = this.details[0]?.lang6_name
     this.languageList[5].speak = this.details[0]?.lang6_speak
     this.languageList[5].read = this.details[0]?.lang6_read
     this.languageList[5].write = this.details[0]?.lang6_write
