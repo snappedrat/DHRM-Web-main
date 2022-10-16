cc26ad9ab56c7782883bbed052da7c4631e0ed91
dashdhanush <dhanushdpandian@gmail.com>
2022-10-16 13:01:06 +0530

    searchfilter function
    needs to be changed, the another filter.

--- src/app/home/rml-user/new-joiners/plantcode.service.ts
+++ src/app/home/rml-user/new-joiners/plantcode.service.ts
@@ -26,6 +26,7 @@ export class PlantcodeService {
   prev : any;
   idforfilename:any
   filterinfo:any
+  searchfilterinfo:any
 
   basicdetails :any = []
   education:any = []
@@ -220,6 +221,17 @@ filter(formvalue:any)
   .post('http://localhost:3000/filter', formvalue)
   .subscribe({
     next: (response) =>{ console.log(response); this.filterinfo = response},
+    error: (error) => console.log(error),
+  });
+}
+
+searchfilter(formvalue:any,input:any)
+{
+  var forms={"value":formvalue,"input":input}
+  this.http
+  .post('http://localhost:3000/searchfilter', forms)
+  .subscribe({
+    next: (response) =>{ console.log(response); this.searchfilterinfo= response},
     error: (error) => console.log(error),
   });
 }
