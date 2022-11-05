b9e64a65ce8e8e7ee953c7a3cd50e1624d14b0ca
aameerk <aameerk917@gmail.com>
2022-11-05 11:51:24 +0530

    commit-newupdates

--- src/app/home/rml-user/training-dojo/question-bank/question-bank.component.ts
+++ src/app/home/rml-user/training-dojo/question-bank/question-bank.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder, UntypedFormGroup, UntypedFormBuilder, UntypedFormControl} from '@angular/forms';
+import { ActivatedRoute, Route } from '@angular/router';
+import { ApiService } from 'src/app/home/api.service';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
+import { threadId } from 'worker_threads';
+import { truncate } from 'fs';
 
 @Component({
-  selector: 'app-question-bank',
+  selector: 'app-trainee-test',
   templateUrl: './question-bank.component.html',
   styleUrls: ['./question-bank.component.css']
 })
 export class QuestionBankComponent implements OnInit {
 
-  constructor() { }
+  form:FormGroup =new FormGroup({})
+  formtest:FormGroup =new FormGroup({})
+
+  loading:any = false
+  modules :any
+  
+
+  username = {'username': this.active.snapshot.paramMap.get('username')}
+
+
+
+  constructor(private fb: UntypedFormBuilder, private service: ApiService, private active: ActivatedRoute, private router:Router) {
+
+
+    this.form = fb.group({
+      module: [''],
+      username: [this.active.snapshot.paramMap.get('username')]
+    })
+   }
 
   ngOnInit(): void {
+    console.log(this.form.value);
+    this.service.getModules(this.username)
+    .subscribe({
+      next: (response)=>{console.log(response); this.modules = response},
+      error: (error)=>console.log(error)
+    })
   }
 
-}
+
+  submit(){
+    console.log("values : ",this.form.value);
+  }
+    
+  }
+
+
+
+
+
+
