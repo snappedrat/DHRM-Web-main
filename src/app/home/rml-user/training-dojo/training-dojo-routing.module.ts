01013c16f3f744cb095b7e9ae7c77d5abd3cf2c4
aameerk <aameerk917@gmail.com>
2022-11-10 09:17:14 +0530

    commit-change

--- src/app/home/rml-user/training-dojo/training-dojo-routing.module.ts
+++ src/app/home/rml-user/training-dojo/training-dojo-routing.module.ts
@@ -4,6 +4,7 @@ import { QuestionBankComponent } from './question-bank/question-bank.component';
 import { TestEvaluationComponent } from './test-evaluation/test-evaluation.component';
 import { TestResultSummaryComponent } from './test-result-summary/test-result-summary.component';
 import { TraineeAnswerComponent } from './trainee-answer/trainee-answer.component';
+import { TraineeOfflineTestComponent } from './trainee-offline-test/trainee-offline-test.component';
 import { TraineeScoreCardComponent } from './trainee-score-card/trainee-score-card.component';
 import { TrainingModulesComponent } from './training-modules/training-modules.component';
 
@@ -31,6 +32,10 @@ const routes: Routes = [
   {
     path: 'trainee-answer',
     component: TraineeAnswerComponent
+  },
+  {
+    path: 'trainee-offline-test',
+    component: TraineeOfflineTestComponent
   }
 ];
 @NgModule({
