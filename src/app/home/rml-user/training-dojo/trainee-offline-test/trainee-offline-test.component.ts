efe0a5dc7abc2542f4580944fe4c7a301fbc93fc
aameerk <aameerk917@gmail.com>
2022-11-10 10:23:31 +0530

    commit-random

--- src/app/home/rml-user/training-dojo/trainee-offline-test/trainee-offline-test.component.ts
+++ src/app/home/rml-user/training-dojo/trainee-offline-test/trainee-offline-test.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TraineeOfflineTestComponent implements OnInit {
 
   constructor() { }
+ 
+
 
   ngOnInit(): void {
   }
--- src/app/home/rml-user/training-dojo/trainee-score-card/trainee-score-card.component.ts
+++ src/app/home/rml-user/training-dojo/trainee-score-card/trainee-score-card.component.ts
@@ -11,5 +11,56 @@ export class TraineeScoreCardComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  dummy :any = [
+    {
+      module_attended:'Induction Training',
+      minpassp:"80.00",
+      pts:"5.00",
+      pts1:"10.00",
+      ptp:"0.00",
+      ptpt:"50.00",
+      ptp1:"100.00",
+      prepf:"Fail",
+      postpf:"Pass",
+      post1pf:"pass"
+    },
+    {
+      module_attended:'safety',
+      minpassp:"80.00",
+      pts:"5.00",
+      pts1:"10.00",
+      ptp:"0.00",
+      ptpt:"50.00",
+      ptp1:"100.00",
+      prepf:"Fail",
+      postpf:"Pass",
+      post1pf:"pass"
+    },
+    {
+      module_attended:'SS',
+      minpassp:"80.00",
+      pts:"5.00",
+      pts1:"10.00",
+      ptp:"0.00",
+      ptpt:"50.00",
+      ptp1:"100.00",
+      prepf:"Fail",
+      postpf:"Pass",
+      post1pf:"pass"
+    },
+    {
+      module_attended:'Fundamental',
+      minpassp:"80.00",
+      pts:"5.00",
+      pts1:"10.00",
+      ptp:"0.00",
+      ptpt:"50.00",
+      ptp1:"100.00",
+      prepf:"Fail",
+      postpf:"Pass",
+      post1pf:"pass"
+    },
+  ]
 }
+
+
