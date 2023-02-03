e7e03d193a3d85d3acb328cb7a4998e01d5753c6
aameerk <aameerk917@gmail.com>
2023-02-03 11:38:00 +0530

    commit-change

--- src/app/home/rml-user/time-office-status/time-office-status.module.ts
+++ src/app/home/rml-user/time-office-status/time-office-status.module.ts
@@ -8,6 +8,21 @@ import { CompOffStatusComponent } from './comp-off-status/comp-off-status.compon
 import { OverTimeStatusComponent } from './over-time-status/over-time-status.component';
 import { LeaveStatusComponent } from './leave-status/leave-status.component';
 import { OdStatusComponent } from './od-status/od-status.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import {MatDividerModule} from "@angular/material/divider";
+import {MatTableModule} from "@angular/material/table";
+import {MatRadioModule} from "@angular/material/radio";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatIconModule} from "@angular/material/icon";
+import {NgxMatTimepickerModule} from "ngx-mat-timepicker";
 
 
 @NgModule({
@@ -20,8 +35,26 @@ import { OdStatusComponent } from './od-status/od-status.component';
     OdStatusComponent
   ],
   imports: [
+    MatNativeDateModule,
     CommonModule,
-    TimeOfficeStatusRoutingModule
-  ]
+    TimeOfficeStatusRoutingModule,
+    CalendarModule.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory,
+    }),
+    MatDividerModule,
+    MatTableModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatIconModule,
+    NgxMatTimepickerModule
+    
+]
 })
 export class TimeOfficeStatusModule { }
