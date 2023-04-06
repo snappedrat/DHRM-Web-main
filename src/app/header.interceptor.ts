939ca0fe5d1254cbc913a4211c4731818c9eef8b
aameerk <aameerk917@gmail.com>
2023-04-06 10:26:03 +0530

    added header interceptor

---
+++ src/app/header.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HeaderInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = sessionStorage.getItem('profains_auth');
+    
+    // If a token is present, add it to the Authorization header
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization:token
+        }
+      });
+    }
+
+    // Pass control to the next interceptor in the chain or to the HTTP client if there are no more interceptors
+    return next.handle(request);
+  }
+}
