9f4ebb0503af501c7e6829e9965d9121fa81aea0
aameerk <aameerk917@gmail.com>
2023-04-06 15:29:21 +0530

    commit

--- src/app/header.interceptor.ts
+++ src/app/header.interceptor.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject,Observable } from 'rxjs';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
-
+public isLoading$: BehaviorSubject<boolean>
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+<<<<<<< Updated upstream
     const token = sessionStorage.getItem('token');
     
+=======
+    const token = sessionStorage.getItem('profains');
+    this.isLoading$ = new BehaviorSubject(true)
+    console.log(this.isLoading$)
+>>>>>>> Stashed changes
     // If a token is present, add it to the Authorization header
     if (token) {
       request = request.clone({
@@ -25,6 +32,12 @@ export class HeaderInterceptor implements HttpInterceptor {
     }
 
     // Pass control to the next interceptor in the chain or to the HTTP client if there are no more interceptors
-    return next.handle(request);
+    return next.handle(request).pipe(tap(
+      event=>{
+        this.isLoading$.next(false)
+        console.log(this.isLoading$)
+      }
+    ))
+    };
   }
-}
+
