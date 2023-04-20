5db54b5350221fc4b85e56261af9d510f5dfb3f0
aameerk <aameerk917@gmail.com>
2023-04-20 13:07:37 +0530

    commit-changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  path: 'http://localhost:3000',
+  path: 'http://localhost:5000',
   path2:'http://localhost:5000',
   // path2:'http://14.99.10.243:3000'
 };
